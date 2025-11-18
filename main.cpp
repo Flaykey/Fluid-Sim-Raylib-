43c280234e3f5cc1ca9303b4af9933b8c83603fc
Flaykey <samratghimire13@gmail.com>
2025-11-18 12:47:28 +0545

    First

--- main.cpp
+++ main.cpp
@@ -1,6 +1,15 @@
 #include<iostream>
-
+#include"include/raylib.h"
 int main(){
-    std::cout<<"Hello world";
+    InitWindow(1366,786,"Hello World");
+    while (!WindowShouldClose())
+    {
+        BeginDrawing();
+        ClearBackground(LIGHTGRAY);
+        DrawCircle(500,500,100,RED);
+        EndDrawing();
+    }
+    
+    CloseWindow();
     return 0;
 }
